02554eaf803c1ade06189ff9b61df34e3b2e525c
veto <krukson17@gmail.com>
2021-07-09 15:29:50 +0200

    Working with python client, not C++

--- Python_AFE_files/main.py
+++ Python_AFE_files/main.py
@@ -1,26 +1,7 @@
 # main.py -- put your code here!
-import time
-import pyb
-import afedrv
-import hub
 import misc
-import ctrl_server
+import afedrv
+import server
 
-print("Jestem w main\n")
-#can = pyb.CAN(1)
-#can.init(pyb.CAN.NORMAL,extframe=False,prescaler=8,sjw=1,bs1=7,bs2=2,auto_restart=True)
-#Set filer - all responses to FIFO 0
-#can.setfilter(0, 0, 0, (0x00,0x7ff))
-#while False:
-    # Change False to True to send the messages in a loop
-#    can.send("1234",0x30)
-#    time.sleep(0.1)
-#    print("tick\n")
-#Send the command to AFE:
-#can.send("\x00\x10",1)
-#Wait and read response
-#time.sleep(1)
-#print(can.recv(0))
 
-server = ctrl_server.ctlsrv()
-server.run(5555)
+print("niech działa")
--- Python_AFE_files/misc.py
+++ Python_AFE_files/misc.py
@@ -3,30 +3,24 @@ import time
 import pyb
 import afedrv
 import hub
-import os 
+import os
+
 
 def HVon(id):
-	pyb.Pin.cpu.E12.init(pyb.Pin.OUT_PP, pyb.Pin.PULL_NONE)
-	pyb.Pin.cpu.E12.value(1)
-	afedrv.SetAllHV(id)
+    pyb.Pin.cpu.E12.init(pyb.Pin.OUT_PP, pyb.Pin.PULL_NONE)
+    pyb.Pin.cpu.E12.value(1)
+    afedrv.SetAllHV(id)
+
 
 def HVoff(id):
-	pyb.Pin.cpu.E12.init(pyb.Pin.OUT_PP, pyb.Pin.PULL_NONE)
-	pyb.Pin.cpu.E12.value(0)
-	afedrv.ClrAllHV(id)
+    pyb.Pin.cpu.E12.init(pyb.Pin.OUT_PP, pyb.Pin.PULL_NONE)
+    pyb.Pin.cpu.E12.value(0)
+    afedrv.ClrAllHV(id)
 
-    
+
 def init(id):
-	afedrv.SetDac(id, 53, 53)
-	afedrv.SetDigRes(id, 0, 200)
-	afedrv.SetDigRes(id, 1, 200)
-	
+    x = afedrv.SetDac(id, 53, 53)
+    afedrv.SetDigRes(id, 0, 200)
+    afedrv.SetDigRes(id, 1, 200)
+    return x
 
-
-
-
-								
-	
-		
-		
-	
